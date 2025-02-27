45f06fc3867660140086c68ffc0901dd790dd541
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-27 13:00:42 -0300

    feat: add closeMenuAttach method to remove menu on outside click

--- src/controller/whatsAppController.js
+++ src/controller/whatsAppController.js
@@ -185,6 +185,53 @@ class WhatsAppController {
 
       })
 
+      this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+        item.on('click', e=> {
+
+            this.el.home.hide();
+            this.el.main.css({
+                display:'flex'
+            })
+
+        })
+
+      })
+
+      this.el.btnAttach.on('click', (e) => {
+
+        e.stopPropagation()
+        this.el.menuAttach.addClass('open')
+        document.addEventListener('click', this.closeMenuAttach.bind(this))
+
+      })
+
+
+      this.el.btnAttachPhoto.on('click', (e) => {
+        
+        console.log('Photo')
+      });
+      this.el.btnAttachCamera.on('click', (e) => {
+        
+        console.log('Camera')
+      });
+      this.el.btnAttachDocument.on('click', (e) => {
+       
+        console.log('Document')
+      });
+      this.el.btnAttachContact.on('click', (e) => {
+       
+        console.log('Contact')
+      });
+
+    }
+
+    closeMenuAttach(e) {
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+        console.log('remove menu')
+
     }
 
     closeAllLeftPanel() {
