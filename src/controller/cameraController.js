57dea8256bec01c85b6a4597265819dd459cc057
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-28 10:12:40 -0300

    feat: add CameraController to handle video streaming

---
+++ src/controller/cameraController.js
@@ -0,0 +1,16 @@
+class CameraController {
+    constructor(videoEl) {
+        this._videoEl = videoEl;
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(stream => {
+            
+            this._videoEl.srcObject = stream; // Correção: usar srcObject
+            this._videoEl.play();
+            
+        }).catch(err => {
+            console.error("Erro ao acessar a câmera:", err);
+        });
+    }
+}
