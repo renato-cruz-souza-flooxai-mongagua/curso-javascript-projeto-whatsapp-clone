2d532b67cf1e9ece721f67259ad2061e46dc1671
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-03-05 13:12:50 -0300

    feat: add stop method and takePicture function in video handling

--- src/controller/cameraController.js
+++ src/controller/cameraController.js
@@ -6,11 +6,33 @@
             video: true
         }).then(stream => {
             
+            this._stream = stream;
             this._videoEl.srcObject = stream; // Correção: usar srcObject
             this._videoEl.play();
             
         }).catch(err => {
             console.error("Erro ao acessar a câmera:", err);
         });
+    }
+
+    stop() {
+
+        this._stream.getTracks().forEach(track => {
+            track.stop()
+        })
+    }
+
+    takePicture(mineType = 'image/png') {
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d')
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height)
+
+        return canvas.toDataURL(mineType)
     }
 }
