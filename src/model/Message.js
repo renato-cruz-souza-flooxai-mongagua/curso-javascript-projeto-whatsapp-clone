bd3102bbf26ffde29b0fd769a77e3d017b3bf6ce
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-03-11 16:18:14 -0300

    feat: add Format module import

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,5 +1,6 @@
 import { Firebase } from "../util/Firebase";
 import { Model } from "./model"
+import { Format } from "../util/format";
 
 export class Message extends Model {
 
@@ -269,16 +270,16 @@ export class Message extends Model {
             
             div.innerHTML = `
 
-                <div class="font-style _3DFk6 tail">
+                <div class="font-style _3DFk6 tail" id= "_${this.id}>
                     <span class="tail-container"></span>
                     <span class="tail-container highlight"></span>
                     <div class="Tkt2p">
                         <div class="_3zb-j ZhF0n">
-                            <span dir="ltr" class="selectable-text invisible-space message-text">Oi!</span>
+                            <span dir="ltr" class="selectable-text invisible-space message-text">${this.content}</span>
                         </div>
                         <div class="_2f-RV">
                             <div class="_1DZAH">
-                                <span class="msg-time">11:33</span>
+                                <span class="msg-time">${Format.timeStampToTime(this.timeStamp)}</span>
                             </div>
                         </div>
                     </div>
