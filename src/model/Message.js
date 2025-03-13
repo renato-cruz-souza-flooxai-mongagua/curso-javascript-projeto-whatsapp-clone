5930a7a55f99528bebfb2aff49d0bf67c13369fc
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-03-13 11:28:34 -0300

    refactor: update HTML structure in message rendering

--- src/model/Message.js
+++ src/model/Message.js
@@ -81,30 +81,48 @@ export class Message extends Model {
 
             
             div.innerHTML = `
-
-
-            <div class="_34Olu">
-                <div class="_2BzIU">
-                    <div class="_2X3l6">
-                        <svg class="_1UDDE" width="50" height="50" viewBox="0 0 43 43">
-                            <circle class="_3GbTq _2wGBy" cx="21.5" cy="21.5" r="20" fill="none" stroke-width="3"></circle>
-                        </svg>
-                    </div>
-                    <div class="_1l3ap">
-                        <span data-icon="media-disabled" class="">
-                            <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" width="44" height="44">
-                                <path fill="#FFF" fill-opacity=".4" d="M29.377 16.099l-1.475-1.475L22 20.525l-5.901-5.901-1.476 1.475L20.525 22l-5.901 5.901 1.476 1.475 5.9-5.901 5.901 5.901 1.475-1.475L23.475 22l5.902-5.901z"></path>
-                            </svg>
-                        </span>
+            <div class="_3_7SH _3qMSo">
+                <div class="KYpDv">
+                    <div>
+                        <div class="_3v3PK" style="width: 330px; height: 330px;">
+                            <div class="_34Olu">
+                                <div class="_2BzIU">
+                                    <div class="_2X3l6">
+                                        <svg class="_1UDDE" width="50" height="50" viewBox="0 0 43 43">
+                                            <circle class="_3GbTq _2wGBy" cx="21.5" cy="21.5" r="20" fill="none" stroke-width="3"></circle>
+                                        </svg>
+                                    </div>
+                                    <div class="_1l3ap">
+                                        <span data-icon="media-disabled" class="">
+                                            <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" width="44" height="44">
+                                                <path fill="#FFF" fill-opacity=".4" d="M29.377 16.099l-1.475-1.475L22 20.525l-5.901-5.901-1.476 1.475L20.525 22l-5.901 5.901 1.476 1.475 5.9-5.901 5.901 5.901 1.475-1.475L23.475 22l5.902-5.901z"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="${this.content}" class="_1JVSX message-photo" style="width: 100%; display:none">
+                            <div class="_1i3Za"></div>
+                        </div>
+                        <div class="_2TvOE">
+                            <div class="_1DZAH text-white" role="button">
+                                <span class="message-time">${Format.timeStampToTime(
+                                    this.timeStamp
+                                  )}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                <div class="_3S8Q-" role="button">
+                    <span data-icon="forward-chat">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="25" height="25">
+                            <path fill="#FFF" d="M14.2 9.5V6.1l5.9 5.9-5.9 6v-3.5c-4.2 0-7.2 1.4-9.3 4.3.8-4.2 3.4-8.4 9.3-9.3z"></path>
+                        </svg>
+                    </span>
+                </div>
             </div>
-            <img src=${this.content} class="_1JVSX message-photo" style="width: 100%; display:none">
-            <div class="_1i3Za"></div>
-        
-
             `;
-
             div.querySelector('.message-photo').on('load', e=>{
 
                console.log('load ok') 
@@ -310,7 +328,7 @@ export class Message extends Model {
    
         }, ()=>{
    
-           Message.sendImage(chatId,
+           Message.send(chatId,
              from, 
              'image',
               uploadTask.snapshot.downloadURL
